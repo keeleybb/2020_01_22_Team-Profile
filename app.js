176317386a846b6ddec7fa5f6f73b2c279b5a85f
kbb <54555434+keeleybb@users.noreply.github.com>
2020-01-18 20:22:12 -0700

    passing all tests

---
+++ app.js
@@ -0,0 +1,16 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const outputPath = path.resolve(__dirname, "output", "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+//Ask First Question
+// inquirer
+//     .prompt(questions)
+
+//Functions to ask for more info on other team members
